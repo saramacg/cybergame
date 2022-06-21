f9b7fe43391e530393ae98f91eb9988d66fd27a3
sara <40476305+saramacg@users.noreply.github.com>
2022-06-21 14:03:10 +1200

    Add files via upload

---
+++ 22.06.01/Game.gd
@@ -0,0 +1,22 @@
+extends Spatial
+
+func _ready():
+	var VR = ARVRServer.find_interface("OpenVR")
+	if VR and VR.initialize():
+		get_viewport().arvr = true
+		get_viewport().hdr = false
+
+		OS.vsync_enabled = false
+		Engine.target_fps = 90
+		# Also, the physics FPS in the project settings is also 90 FPS. This makes the physics
+		# run at the same frame rate as the display, which makes things look smoother in VR!
+
+
+var spheres_left = 10
+var sphere_ui = null
+
+func remove_sphere():
+	spheres_left -= 1
+	
+	if sphere_ui:
+		sphere_ui.update_ui(spheres_left)
